c6f715bbc9477f1c806e573577be31bcd31fb1ae
johnoro <officialjohnorourke@gmail.com>
2019-05-03 16:42:25 -0400

    Adds a model solution for knapsack for own reference

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -17,6 +17,53 @@ def knapsack_solver(items, capacity):
       total_weight += weight
   best['Chosen'].sort()
   return best
+
+  # passes the tests
+  # """
+  # Correct but very memory inefficient solution, can only solve problems where
+  # computer memory is at least number of items times capacity time 8bytes large
+  # """
+  # # Initialize the cache matrix: rows are items (including a "no item" row)
+  # # Columns are integral capacities: 0, 1, 2, ..., capacity - 1, capacity
+  # cache = [[0] * (capacity + 1) for _ in range(len(items) + 1)]
+
+  # # Our bag represented as a Set data structure
+  # bag = set()
+
+  # # DP loops for filling in cache matrix
+  # # if the item does not fit in a "sub-knapsack", don't include it
+  # # else include it only if its value makes the knapsack more valuable
+  # # These loops fill the matrix but do not solve the problem
+  # # Note - first row is all 0s because picking from 0 items
+  # # Need to have it to refer back to for solution computation
+  # for item in range(1, len(cache)):
+  #   for size in range(len(cache[item])):
+  #     if items[item-1].size > size:
+  #       # Item doesn't fit, skip
+  #       cache[item][size] = cache[item-1][size]
+  #     else:
+  #         # Item fits, take max of skipping it or adding it
+  #         # Adding -> base new knapsack on current - size + value of item
+  #       cache[item][size] = max(
+  #         cache[item-1][size],
+  #         cache[item-1][size - items[item-1].size] + items[item-1].value)
+
+  # # With this loop we can traverse the matrix and figure out what items
+  # # to take in our knapsack, that is, if the value increased from item i-1
+  # # to item i then we know we took the item and should go to the previous
+  # # item in the smaller knapsack
+  # # (the column discounting the size of the taken item)
+  # rows = len(cache) - 1
+  # cols = len(cache[-1]) - 1
+  # while rows > 0 and cols > 0:
+  #   if cache[rows][cols] != cache[rows-1][cols]:
+  #     bag.add(rows - 1)
+  #     rows -= 1
+  #     cols -= items[rows].size
+  #   else:
+  #     rows -= 1
+
+  # return dict(Value=cache[-1][-1], Chosen=list(sorted(map(lambda i: i+1, bag))))
   
 
 if __name__ == '__main__':
